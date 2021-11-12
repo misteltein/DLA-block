c016baaa7e564870f3b3d98b19915a0d685c8db9
misteltein <itogahibiki@gmail.com>
2021-11-13 00:50:38 +0900

    コメント追加

--- src/Square.ts
+++ src/Square.ts
@@ -1,31 +1,50 @@
 import P5 from "p5";
-import Particle from "./Particle";
+import Circle from "./Circle";
 
+/**
+ * 正方形のクラス
+ */
 export default class Square {
-  static size: number;
-
+  // 幅
+  static SIZE: number;
+  // 中心座標（デフォルトの座標系）
   xCenter: number;
   yCenter: number;
+  // 色で塗られているか
   filled: boolean;
 
+  /**
+   * 正方形をつくる
+   * @param x - 左上の角の x 座標
+   * @param y - 右上の角の y 座標
+   */
   constructor(x: number, y: number) {
-    this.xCenter = x+ 0.5 * Square.size;
-    this.yCenter = y+ 0.5 * Square.size;
+    this.xCenter = x + 0.5 * Square.SIZE;
+    this.yCenter = y + 0.5 * Square.SIZE;
     this.filled = false;
   }
 
-  update(particle: Particle) {
-    if (this.filled || particle.ongoing) return;
-    const xParticle: number = particle.getX();
-    const yParticle: number = particle.getY();
+  /**
+   * Circle が正方形の中心を含むときは塗りつぶすためのサブルーチン
+   * @param {Circle} circle
+   */
+  updateState(circle: Circle) {
+    if (this.filled || circle.ongoing) return;
+    const xCircle: number = circle.getX();
+    const yCircle: number = circle.getY();
     this.filled =
       Math.sqrt(
-        Math.pow(this.xCenter - xParticle, 2) + Math.pow(this.yCenter - yParticle, 2)
-      ) <= particle.r;
+        Math.pow(this.xCenter - xCircle, 2) +
+          Math.pow(this.yCenter - yCircle, 2)
+      ) <= circle.r;
   }
 
+  /**
+   * 描画
+   * @param {P5} p5
+   */
   draw(p5: P5) {
-    p5.rectMode(p5.CENTER)
-    p5.rect(this.xCenter, this.yCenter, Square.size, Square.size);
+    p5.rectMode(p5.CENTER);
+    p5.rect(this.xCenter, this.yCenter, Square.SIZE, Square.SIZE);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 // https://editor.p5js.org/misteltein/sketches/pmp6gJ0cV
 import P5 from "p5";
-import Particle from "./Particle";
+import Circle from "./Circle";
 import Square from "./Square";
 
 // setup / draw で繰り返し使用する配列
 const colors: Array<P5.Color> = [];
-const particles: Array<Particle> = [];
+const circles: Array<Circle> = [];
 const squares: Array<Square> = [];
 
 // 画面いっぱいに図形が充填されるまで継続するためのフラグ
@@ -23,66 +23,69 @@ const sketch = (p5: P5) => {
 
   p5.setup = () => {
     p5.createCanvas(1000, 600);
-    // Particle の座標系の原点をカンバスの中心に
-    Particle.OX = p5.width * 0.5;
-    Particle.OY = p5.height * 0.5;
-    // Particle を生成するリングの半径
-    Particle.R =
+    // Circle の座標系の原点をカンバスの中心に
+    Circle.OX = p5.width * 0.5;
+    Circle.OY = p5.height * 0.5;
+    // Circle を生成するリングの半径
+    Circle.R =
       Math.sqrt(Math.pow(p5.width * 0.5, 2) + Math.pow(p5.height * 0.5, 2)) +
       65;
-    // カンバス中心に，止まった particle を生成
-    const origin: Particle = new Particle();
-    origin.dX = 0.0;
-    origin.dY = 0.0;
+    // カンバス中心に，止まった円を生成
+    const origin: Circle = new Circle();
+    origin.x = 0.0;
+    origin.y = 0.0;
     origin.r = 10.0;
     origin.ongoing = false;
-    particles.push(origin);
-    // １つめの動く particle を生成
-    particles.push(new Particle());
+    circles.push(origin);
+    // １つめの動く円を生成
+    circles.push(new Circle());
 
     // 正方形を敷き詰めるように生成
     const RESOLUTION = 40;
-    Square.size = Math.max(p5.width, p5.height) / RESOLUTION;
+    Square.SIZE = Math.max(p5.width, p5.height) / RESOLUTION;
     for (let i = 0; i < RESOLUTION; ++i) {
       for (let j = 0; j < RESOLUTION; ++j) {
-        squares.push(new Square(i * Square.size, j * Square.size));
+        squares.push(new Square(i * Square.SIZE, j * Square.SIZE));
       }
     }
   };
 
   p5.draw = () => {
-    // 正方形の描画
+    // 正方形の描画（デフォルトの座標系）
     p5.noStroke();
     squares.forEach((square: Square, idx) => {
       p5.fill(square.filled ? colors[idx % colors.length] : p5.color(220));
       square.draw(p5);
     });
 
-    // 円の描画ここから
+    // 円の描画（カンバスを中心とする座標系）ここから
     p5.push();
-    p5.translate(Particle.OX, Particle.OY);
+    p5.translate(Circle.OX, Circle.OY);
     p5.stroke(255);
     p5.strokeWeight(1);
-    particles.forEach((particle: Particle, i: number) => {
+    circles.forEach((circle: Circle, i: number) => {
       p5.fill(colors[i % colors.length]);
-      particle.draw(p5);
-      particle.collapse(i, particles);
+      circle.draw(p5);
+      circle.updateState(i, circles);
       if (ongoing) {
-        particle.evolution();
+        circle.evolution();
       }
-    });
-    squares.forEach((square: Square) => {
-      particles.forEach((particle: Particle) => {
-        square.update(particle);
-      });
     });
     p5.pop();
     // 円の描画ここまで
 
-    // 円の追加
-    if (!particles[particles.length - 1].ongoing) {
+    // 正方形の色を更新
+    squares.forEach((square: Square) => {
+      circles.forEach((circle: Circle) => {
+        square.updateState(circle);
+      });
+    });
+
+    // 最後に追加した円が静止したら新しく円を追加
+    if (!circles[circles.length - 1].ongoing) {
       try {
-        particles.push(new Particle());
+        // 追加してすぐに接触していたら例外が発生するので描画を停止させる
+        circles.push(new Circle());
       } catch (error) {
         ongoing = false;
       }
